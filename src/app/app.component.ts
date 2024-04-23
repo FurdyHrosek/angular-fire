05416ea18c7a1d7e9d6a59c05a12220c2fd876d7
[Martin Houdek] <mhoudek11@gmail.com>
2024-04-23 19:27:30 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,99 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, addDoc, collection, collectionData, doc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'furdy-fire';
+  isUpdating = false;
+  isUserDataLoading = true;
+  userData!: Observable<any>;
+
+  isPasswordChanging = false;
+  currentUserId?: string;
+  currentPass?: string;
+  passwordChangeMessage: string | null = null;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+  addData(f: any) {
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, f.value)
+      .then(() => console.log('Data Saved Successfully'))
+      .catch(console.error);
+  }
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'users');
+    this.isUserDataLoading = true;
+  
+    collectionData(collectionInstance, { idField: 'id' }).subscribe({
+      next: () => {
+        this.isUserDataLoading = false;
+      },
+      complete: () => {},
+      error: (err) => {
+        console.error(err);
+        this.isUserDataLoading = false;
+      }
+    });
+  
+    this.userData = collectionData(collectionInstance, { idField: 'id' });
+  }
+
+  async updateData(id: string, field: string, newValue: string, currentPassword?: string) {
+    this.isUpdating = true;
+    const userDocRef = doc(this.firestore, 'users', id);
+    const updateData: any = { [field]: newValue };
+
+    try {
+      if (field === 'password' && currentPassword !== this.currentPass) {
+        this.passwordChangeMessage = 'Current password does not match';
+      } else {
+        await updateDoc(userDocRef, updateData);
+        if (field === 'password') {
+          this.passwordChangeMessage = 'Password Updated';
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.isUpdating = false;
+    }
+  }
+
+  deleteData(id: string) {
+    const docInstance = doc(this.firestore, 'users', id);
+    deleteDoc(docInstance)
+      .then(() => console.log('Data Deleted'))
+      .catch(console.error);
+  }
+
+
+  showPassword(inputField: HTMLInputElement) {
+    inputField.type = inputField.type === 'password' ? 'text' : 'password';
+  }
+
+  togglePasswordChanging(selectedValue: string, userId: string, currentPass: string) {
+    if (selectedValue === 'password') {
+      this.isPasswordChanging = true;
+      this.currentUserId = userId;
+      this.currentPass = currentPass;
+    }
+  }
+
+  closePasswordPopup() {
+    this.isPasswordChanging = false;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB6oI5JJf-uSP9-0dNuOjfsNArydtK3P4",
+  authDomain: "furdy-fire.firebaseapp.com",
+  projectId: "furdy-fire",
+  storageBucket: "furdy-fire.appspot.com",
+  messagingSenderId: "330431681247",
+  appId: "1:330431681247:web:1d15c84ed49f19bff08f6a"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))),
+    importProvidersFrom(provideFirestore(() => getFirestore()))
+  ]
 };
